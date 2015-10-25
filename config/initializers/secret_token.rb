3146d7caf3389726b7f2d896e463e5d66665c22f
Art <artvasart@gmail.com>
2015-10-25 17:18:49 +0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SvBlog::Application.config.secret_key_base = 'a9683401e152779254f3329779c089971de68f83d0c8220a0bec1e8954ba139f192f0645394d4f70c64d5704cca836cac419eb99f589358a65ae17313f42adb1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SvBlog::Application.config.session_store :cookie_store, key: '_sv_blog_session'
