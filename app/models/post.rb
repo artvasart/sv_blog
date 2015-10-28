cf63571893e0c5aa08e5bb567e40a1f214a6f172
Art <artvasart@gmail.com>
2015-10-28 22:13:21 +0400

    Added frindly_id with babosa

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
+require "babosa"
+
 class Post < ActiveRecord::Base
+	 extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  def normalize_friendly_id(input)
+    input.to_s.to_slug.normalize(transliterations: :russian).to_s
+  end
 end
