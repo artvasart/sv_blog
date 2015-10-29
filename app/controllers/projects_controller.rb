f42dd0e356f0302545888e3bff8a3a8c81c47d6b
Art <artvasart@gmail.com>
2015-10-29 23:48:10 +0400

    Finish projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,6 +22,23 @@ class ProjectsController < ApplicationController
 		
 	end
 
+	def edit
+		
+	end
+
+	def update
+		if @project.update project_params
+			redirect_to @project, notice: "Project was successfully updated"
+		else
+			render 'edit', alert: "Can't update the project"
+		end
+	end
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path
+	end
+
 	private
 
 	def find_project
