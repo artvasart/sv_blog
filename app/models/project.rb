f2c465e353f1bf1bfec275e76cd5cff4d5753086
Art <artvasart@gmail.com>
2015-10-29 23:32:42 +0400

    Added friendly_id's for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ActiveRecord::Base
+	 extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  def normalize_friendly_id(input)
+    input.to_s.to_slug.normalize(transliterations: :russian).to_s
+  end
 end
